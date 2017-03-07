120a1c078e84158b88ed7562c0ffbcc621eefab9
Tomaslaz <tomas.lazauskas@gmail.com>
2017-03-07 10:42:55 +0000

    Reading in the systems and performing initial checks to make sure that the systems are compatible

--- DA_Find_Defects.py
+++ DA_Find_Defects.py
@@ -8,6 +8,32 @@ A script to identify defects by comparing initial and final systems
 """
 
 from optparse import OptionParser
+import numpy as np
+
+import source.IO as IO
+
+def check_systems(input_system, final_system):
+  """
+  Checks whether the analysis can be performed.
+  
+  """
+  
+  success = True
+  error = ""
+  
+  # checking whether the cell dimensions match
+  if not np.array_equal(input_system.cellDims, final_system.cellDims):
+    success = False
+    error = "Input and final systems do not have matching cell dimensions"
+    
+    return success, error
+  
+  # checking if the number of atoms matches
+  if input_system.NAtoms != final_system.NAtoms:
+    success = False
+    error = "Input and final systems do not have identical number of atoms"
+  
+  return success, error
 
 def cmdLineArgs():
   """
@@ -28,6 +54,49 @@ def cmdLineArgs():
 
   return options, args
 
+def read_systems(input_file, final_file):
+  """
+  Reads in input and final systems
+  
+  """
+  
+  success = True
+  error = ""
+  
+  input_system = None
+  final_system = None
+  
+  # reading in the initial system
+  input_system, error = IO.readSystemFromFile(input_file)
+  
+  if len(error) > 0:
+    success = False
+  
+  if success:
+    # reading in the final system
+    final_system, error = IO.readSystemFromFile(final_file)
+    
+    if len(error) > 0:
+      success = False
+  
+  return success, error, input_system, final_system
+
 if __name__ == "__main__":
   
+  # command line arguments and options
   options, args = cmdLineArgs()
+  
+  # reading in the systems
+  success, error, input_system, final_system = read_systems(args[0], args[1])
+  
+  if success:
+    success, error = check_systems(input_system, final_system)
+  
+  if success:
+    vac_radius = np.float(args[2])
+  
+  if success:
+    print "Finished!"
+  else:
+    print "ERROR: %s" % (error)
+   
